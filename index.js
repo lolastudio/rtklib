7d3cdb5b5e4976462e4707888067ad17990aa16e
Roque Francisco <roquefsd@gmail.com>
2020-10-24 22:38:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+fs.readFile('./local.conf', (err, data) => {
+    let lines = data.toString().split('\r');
+    let parsed = {};
+    for(let l of lines) {
+        l = l.split('\n').join('');
+        l = l.split('	').join('')
+        l = l.split(' ').join('');
+        l = l.split('#');
+        l = l[0]
+        if(l) {
+            l = l.split('=')
+            parsed[l[0]] = l[1] && !isNaN(l[1]) ? +l[1] : l[1];
+        }
+    }
+    console.log(parsed)
+})
