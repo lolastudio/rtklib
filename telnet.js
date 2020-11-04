b30def52573ea550995e086818f075fdf953d4ee
Roque Francisco <roquefsd@gmail.com>
2020-11-04 12:42:02 -0300

    telnet and tests

--- telnet.js
+++ telnet.js
@@ -2,26 +2,29 @@ const telnet = require('telnet-client')
 
 class Telnet {
     constructor(options) {
-        this.params = {
+        this.params = Object.assign({
             host: 'localhost',
             port: 3200,
             shellPrompt: 'rtkrcv> ',
             timeout: 3000,
             negotiationMandatory: false,
             ors: '\r\n',
-            waitfor: '\n'
-        }
+            waitfor: '\n',
+            password: 'admin'
+        }, options)
 
+        this.connect = this.connect.bind(this);
+        this.disconnect = this.disconnect.bind(this);
+        this.exec = this.exec.bind(this);
         return this;
     }
 
     connect() {
-        let connection = new telnet()
+        this.connection = new telnet()
         return new Promise((resolve, reject) => {
-            connection.connect(this.params).then(() => {
-                connection.exec('admin').then((res) => {
-                    console.log('login:', res);
-                    resolve(connection);
+            this.connection.connect(this.params).then(() => {
+                this.connection.exec(this.params.password).then((res) => {
+                    resolve(this.connection);
                 }).catch(err => {
                     console.error(err)
                 });
@@ -29,23 +32,30 @@ class Telnet {
         });
     }
 
+    disconnect() {
+        return this.connection.end();
+    }
+
     exec(cmd) {
         return new Promise((resolve, reject) => {
             this.connection.exec(cmd).then(res => {
-                resolve(this.parse(res));
+                cmd == 'status' ? resolve(this.parse(res)) : resolve(res);
             });
         });
     }
 
     parse(str = '', header = true) {
-        str = str.split('\x1B[0m').join('').split('\x1B[1m').split('\u001b[1m').join('').split('\u001b[0m');
-        let lines = str[1].split('\n');
+        str = str.split('\x1B[0m').join('').split('\x1B[1m').join('').split('\u001b[1m').join('').split('\u001b[0m');
+        let lines = (str[1] || str[0]).split('��\x03��\x01').join('').split('\n');
         let result = {}
-        for(let l = header ? 1 : 0; l < lines.length; l++) {
+        for (let l = header ? 1 : 0; l < lines.length; l++) {
             let line = lines[l]
             line = line.split(': ');
             line[0] = line[0].split('  ').join(' ').split('  ').join('')
-            result[line[0]] = line[1];
+            line[0] = line[0].endsWith(' ') ? line[0].substring(0, line[0].length - 1) : line[0]
+            if (line[0] && line[0] != 'Parameter') {
+                result[line[0]] = line[1];
+            }
         }
         return result;
     }
--- test.js
+++ test.js
@@ -1,22 +1,40 @@
 const fs = require('fs');
 const rtklib = require('./index.js');
 
-fs.readFile('test.conf', (err, data) => {
-    let json = rtklib.config2JSON(data);
-    let string = rtklib.JSON2config(json);
+function parser_test() {
+    fs.readFile('test.conf', (err, data) => {
+        let json = rtklib.config2JSON(data);
+        let string = rtklib.JSON2config(json);
 
-    fs.writeFile('result.conf', string, (err) => {
-        if(err) throw new Error(err);
+        fs.writeFile('result.conf', string, (err) => {
+            if (err) throw new Error(err);
 
-        fs.readFile('result.conf', (err, data) => {
-            let newjson = rtklib.config2JSON(data);
+            fs.readFile('result.conf', (err, data) => {
+                let newjson = rtklib.config2JSON(data);
 
-            for(let key in json) {
-                if(newjson[key] !== json[key]) throw new Error(key);
-            }
-
-            console.log('\x1b[32m' ,'OK')
-            console.log('\x1b[0m', '');
+                for (let key in json) {
+                    if (newjson[key] !== json[key]) throw new Error(key);
+                }
+            });
         });
     });
-});
+}
+
+function telnet_test() {
+    let telnet = new rtklib.telnet({ host: '192.168.0.140' });
+    telnet.connect().then(() => {
+        telnet.exec('status').then(response => {
+            if(Object.keys(response).length < 48) {
+                throw new Error('Telnet status error')
+            }
+            else {
+                telnet.disconnect();
+                console.log('\x1b[32m', 'OK')
+                console.log('\x1b[0m', '');
+            }
+        })
+    });
+}
+
+parser_test();
+telnet_test();
